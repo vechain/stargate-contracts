6d4f56d105524db0d5f146026c23573c45e2dbe0
Dan Rusnac <rusnac14@gmail.com>
2025-06-27 10:53:38 +0200

    chore: set addresses to zero

--- packages/config/mainnet.ts
+++ packages/config/mainnet.ts
@@ -2,14 +2,13 @@ import { defaultMainNetwork } from "@repo/constants";
 import { AppConfig } from ".";
 const config: AppConfig = {
   environment: "mainnet",
-  basePath: "https://aafactory.org",
+  basePath: "https://stargate.org",
   ipfsPinningService: "https://api.gateway-proxy.vechain.org/api/v1/pinning/pinFileToIPFS",
   ipfsFetchingService: "https://api.gateway-proxy.vechain.org/ipfs",
   legacyNodesContractAddress: "0xb81E9C5f9644Dec9e5e3Cac86b4461A222072302",
-  stargateNFTContractAddress: "0xC06Ad8573022e2BE416CA89DA47E8c592971679A",
+  stargateNFTContractAddress: "0x0000000000000000000000000000000000000000",
   nodeManagementContractAddress: "0xB0EF9D89C6b49CbA6BBF86Bf2FDf0Eee4968c6AB",
-  stargateDelegationContractAddress:
-    "0xC06Ad8573022e2BE416CA89DA47E8c592971679A",
+  stargateDelegationContractAddress: "0x0000000000000000000000000000000000000000",
   nodeUrl: "https://mainnet.vechain.org",
   network: defaultMainNetwork,
 };
