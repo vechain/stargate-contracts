32dd7d66bfb8a1bbfcb476b7b2898165ad31ea13
Dan Rusnac <rusnac14@gmail.com>
2025-07-16 16:07:39 +0200

    fix: rewards accumulation start block

---
+++ packages/contracts/scripts/upgrade/upgrades/stargate-delegation/stargate-delegation-v2.ts
@@ -0,0 +1,43 @@
+import { getConfig } from "@repo/config";
+import { upgradeProxy } from "../../../helpers";
+import { EnvConfig } from "@repo/config/contracts";
+import { StargateDelegation } from "../../../../typechain-types";
+import { network } from "hardhat";
+
+async function main() {
+  if (!process.env.VITE_APP_ENV) {
+    throw new Error("Missing VITE_APP_ENV");
+  }
+
+  const config = getConfig(process.env.VITE_APP_ENV as EnvConfig);
+
+  console.log(
+    `Upgrading StargateDelegation contract at address: ${config.stargateDelegationContractAddress} on network: ${config.network.name} with hardhat network set to: ${network.name}`
+  );
+
+  const stargateDelegationV2 = (await upgradeProxy(
+    "StargateDelegation",
+    "StargateDelegation",
+    config.stargateDelegationContractAddress,
+    [],
+    {
+      version: 2,
+    }
+  )) as StargateDelegation;
+
+  console.log(`StargateDelegation upgraded`);
+
+  // check that upgrade was successful
+  const version = await stargateDelegationV2.version();
+  console.log(`New StargateDelegation version: ${version}`);
+
+  if (version !== 2n) {
+    throw new Error(`StargateDelegation version is not 2: ${version}`);
+  }
+
+  console.log("Execution completed");
+  process.exit(0);
+}
+
+// Execute the main function
+main();
