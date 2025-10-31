79a0a793ab493a4fd6aeac215b3444bcdfe597ca
josemarinas <36479864+josemarinas@users.noreply.github.com>
2025-10-31 16:29:15 +0100

    chore: update testnet contract addresses and levels (#22)

--- packages/config/contracts/envs/testnet.ts
+++ packages/config/contracts/envs/testnet.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "../defineConfig";
-import { initialTokenLevels } from "../StargateNFT/testnet";
 import { ethers } from "ethers";
+import { BLOCKS_PER_DAY } from "../type";
+import { TokenLevelId } from "../StargateNFT";
 
 export function createTestnetConfig() {
   return defineConfig({
@@ -59,8 +60,132 @@ export function createTestnetConfig() {
     // Stargate NFT contract
     TOKEN_COLLECTION_NAME: "StarGate Delegator Token",
     TOKEN_COLLECTION_SYMBOL: "SDT",
-    TOKEN_LEVELS: initialTokenLevels,
-    LEGACY_LAST_TOKEN_ID: 100000,
+    TOKEN_LEVELS: [
+      // Legacy normal levels
+      {
+        level: {
+          id: TokenLevelId.Strength,
+          name: "Strength",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("1000000"),
+          scaledRewardFactor: 150,
+          maturityBlocks: BLOCKS_PER_DAY * 30,
+        },
+        cap: 1382, // 2500 - (1100 + 18 None upgrading),
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.Thunder,
+          name: "Thunder",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("5000000"),
+          scaledRewardFactor: 250,
+          maturityBlocks: BLOCKS_PER_DAY * 45,
+        },
+        cap: 234, // 300 - (60 + 6 Strength upgrading)
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.Mjolnir,
+          name: "Mjolnir",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("15000000"),
+          scaledRewardFactor: 350,
+          maturityBlocks: BLOCKS_PER_DAY * 60,
+        },
+        cap: 13, // 100 - (82 + 5 Thunder upgrading)
+        circulatingSupply: 0,
+      },
+      // Legacy X Levels
+      {
+        level: {
+          id: TokenLevelId.VeThorX,
+          name: "VeThorX",
+          isX: true,
+          vetAmountRequiredToStake: ethers.parseEther("600000"),
+          scaledRewardFactor: 200,
+          maturityBlocks: 0,
+        },
+        cap: 0, // 735
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.StrengthX,
+          name: "StrengthX",
+          isX: true,
+          vetAmountRequiredToStake: ethers.parseEther("1600000"),
+          scaledRewardFactor: 300,
+          maturityBlocks: 0,
+        },
+        cap: 0, // 831 + 12 VeThorX upgrading is 843
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.ThunderX,
+          name: "ThunderX",
+          isX: true,
+          vetAmountRequiredToStake: ethers.parseEther("5600000"),
+          scaledRewardFactor: 400,
+          maturityBlocks: 0,
+        },
+        cap: 0, // 175 + 12 StrengthX upgrading is 187
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.MjolnirX,
+          name: "MjolnirX",
+          isX: true,
+          vetAmountRequiredToStake: ethers.parseEther("15600000"),
+          scaledRewardFactor: 500,
+          maturityBlocks: 0,
+        },
+        cap: 0, // 152 + 6 ThunderX upgrading is 158
+        circulatingSupply: 0,
+      },
+      // New levels
+      {
+        level: {
+          id: TokenLevelId.Dawn,
+          name: "Dawn",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("10000"),
+          scaledRewardFactor: 100,
+          maturityBlocks: BLOCKS_PER_DAY * 2,
+        },
+        cap: 500000,
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.Lightning,
+          name: "Lightning",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("50000"),
+          scaledRewardFactor: 115,
+          maturityBlocks: BLOCKS_PER_DAY * 5,
+        },
+        cap: 100000,
+        circulatingSupply: 0,
+      },
+      {
+        level: {
+          id: TokenLevelId.Flash,
+          name: "Flash",
+          isX: false,
+          vetAmountRequiredToStake: ethers.parseEther("200000"),
+          scaledRewardFactor: 130,
+          maturityBlocks: BLOCKS_PER_DAY * 15,
+        },
+        cap: 25000,
+        circulatingSupply: 0,
+      },
+    ],
+    LEGACY_LAST_TOKEN_ID: 15611,
     BASE_TOKEN_URI: "ipfs://bafybeibmpgruasnoqgyemcprpkygtelvxl3b5d2bf5aqqciw6dds33yw7y/metadata/",
     WHITELIST_ENTRIES_V2: [], // Overwritten on deploy
     // NodeManagement contract
